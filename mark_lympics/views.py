e18ed1939e3184f4fc48a5249f32fcfbb8a0ab21
Jorge Titoneli <jorgeltpf@gmail.com>
2016-08-04 23:17:51 -0300

    Login and registration creation

--- mark_lympics/views.py
+++ mark_lympics/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.context_processors import csrf
+from .forms import UserRegistrationForm
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'mark_lympics/index.html', {})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/accounts/register_success')
+    args = {}
+    args.update(csrf(request))
+    args['form'] = UserRegistrationForm()
+    return render(request, 'mark_lympics/register.html', args)

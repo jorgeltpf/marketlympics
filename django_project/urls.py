345be14f5e8ad7c66d3438316041913beed6e0b6
Jorge Titoneli <jorgeltpf@gmail.com>
2016-08-20 18:03:48 -0300

    fix: implementação do login

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,3 +1,4 @@
+
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views
@@ -6,8 +7,7 @@ from mark_lympics.forms import LoginForm
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('mark_lympics.urls')),
-    url(r'^login/$', views.login, {
-        'template_name': 'login.html', 'authentication_form': LoginForm},
+    url(r'^login/$', views.login, {'template_name': 'login.html'},
         name='login'),
     url(r'^logout/$', views.logout, {'next_page': '/login'}),
 ]
